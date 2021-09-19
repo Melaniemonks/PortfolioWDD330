8f8b4dede5e330a6a9cb2003b7044f8ad3102d68
Melanie Monkurai <Mon17045@byui.edu>
2021-09-18 22:49:43 -0600

    initial commit

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const links = [
     {
       label: "Week1 notes",
-      url: "week1/index.html"
+      url: "week01/index.html"
     }
   ]
 
@@ -20,17 +20,3 @@ for (let i=0 ; i < links.length; i++){
 }
 
 
-function loadStory(){
-  var storyName = document.getElementById("name_input").value;
-  var storyHTML = localStorage(storyName);
-  document.getElementById("story_editor").value=storyHTML;
-}
-function saveStory(){
-  var storyName = document.getElementById("name_input").value;
-  var storyHTML = document.getElementById("story_editor").value;
-  localStorage.setItem(storyName, storyHTML);
-}
-function displayStory(){
-  var storyHTML = document.getElementById("story_editor").value;
-  document.getElementById("story_display").innerHTML = storyHTML;
-}
